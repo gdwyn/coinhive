4d7db1ee2424b87e7a248ab20069834b8fc97583
Godwin Ebri <godwinnebri@gmail.com>
2024-09-19 13:40:33 +0100

    ⚡️ let there be code

---
+++ coinhive/coinhiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  coinhiveApp.swift
+//  coinhive
+//
+//  Created by Godwin IE on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct coinhiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
