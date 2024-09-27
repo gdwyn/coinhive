79859b15f835eb143778e43efc5d97ce4a741969
Godwin Ebri <godwinnebri@gmail.com>
2024-09-27 04:32:01 +0100

    custom error handling

--- coinhive/Core/All Coins/Service/CoinDataService.swift
+++ coinhive/Core/All Coins/Service/CoinDataService.swift
@@ -10,23 +10,38 @@ import Foundation
 class CoinDataService {
     let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=40&page=1&sparkline=false&price_change_percentage=24h&precision=2"
     
-    func fetchCoins(completion: @escaping(Result<[Coin], Error>) -> Void) {
+    func fetchCoins(completion: @escaping(Result<[Coin], CoinAPIError>) -> Void) {
         guard let url = URL(string: urlString) else { return }
         
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
-                completion(.failure(error))
+                completion(.failure(.unknownError(error: error)))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.requestFailed(description: "Request failed")))
+                return
+            }
+           
+            guard httpResponse.statusCode == 200 else {
+                completion(.failure(.invalidStatusCode(code: httpResponse.statusCode)))
                 return
             }
 
-            guard let data = data else { return }
-            
-            guard let coins = try? JSONDecoder().decode([Coin].self, from: data) else {
-                print("DEBUG: failed to fetch coins")
+            guard let data = data else {
+                completion(.failure(.invalidData))
                 return
             }
             
-            completion(.success(coins))
+            do {
+                let coins = try? JSONDecoder().decode([Coin].self, from: data)
+                completion(.success(coins))
+            } catch {
+                print("DEBUG: error, \(error.localizedDescription)")
+                completion(.failure(.jsonParsingFailure))
+            }
+
             
         }.resume()
     }
