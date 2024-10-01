916f4b993069ce2208c8c34e39757c1270c9b2e2
Godwin Ebri <godwinnebri@gmail.com>
2024-10-01 18:14:05 +0100

    fixed coin list ui bug
    some weird spacing caused by the sparkline in the coin view

--- coinhive/Core/All Coins/View/CoinList.swift
+++ coinhive/Core/All Coins/View/CoinList.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct CoinList: View {
     var coins: [Coin]
-
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text("Coins")
@@ -19,7 +19,7 @@ struct CoinList: View {
             LazyVStack(alignment: .leading, spacing: 8) {
                 ForEach(coins) { coin in
                     VStack {
-                        HStack(spacing: 0) {
+                        HStack {
                             HStack(spacing: 12) {
                                 AsyncImage(url: URL(string: coin.image)) { image in
                                     image
@@ -42,34 +42,31 @@ struct CoinList: View {
                                         .font(.caption)
                                         .foregroundStyle(.gray)
                                 }
-                            }
-                            .frame(width: 140, alignment: .leading)
-                            
-                            Spacer()
-                            
-                            VStack(alignment: .leading, spacing: 4) {
-                                Text(coin.currentPrice, format: .currency(code: "GBP"))
-                                    .font(.subheadline)
+                                .frame(width: 80, alignment: .leading)
                                 
-                                HStack(spacing: 2) {
-                                    Image(systemName: coin.priceChangePercentage >= 0 ? "chevron.up" : "chevron.down")
-                                    Text(String(format: "%.2f", coin.priceChangePercentage))
+                                VStack(alignment: .leading, spacing: 4) {
+                                    Text(coin.currentPrice, format: .currency(code: "GBP"))
+                                        .font(.subheadline)
+                                    
+                                    HStack(spacing: 2) {
+                                        Image(systemName: coin.priceChangePercentage >= 0 ? "chevron.up" : "chevron.down")
+                                        Text(String(format: "%.2f", coin.priceChangePercentage))
+                                    }
+                                    .foregroundColor(coin.priceChangePercentage >= 0 ? .green : .red)
+                                    .font(.caption)
+                                    
                                 }
-                                .foregroundColor(coin.priceChangePercentage >= 0 ? .green : .red)
-                                .font(.caption)
-                                
                             }
                             
                             Spacer()
-
+                            
                             SparklineView(sparkline: coin.sparkLine.price, maxVisiblePoints: 30, lineColor: coin.priceChangePercentage >= 0 ? .green : .red)
                                 .background(coin.priceChangePercentage >= 0 ? .green.opacity(0.04) : .red.opacity(0.04))
                                 .clipShape(RoundedRectangle(cornerRadius: 12))
                                 .frame(width: 78, height: 48)
-
+                            
                         }
                         .padding()
-
                         
                         Divider()
                             .frame(alignment: .bottom)
